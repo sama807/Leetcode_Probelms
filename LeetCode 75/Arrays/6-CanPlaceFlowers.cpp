664883689fcdef7d51eaf3ba706e4543c5e347b0
Sama807 <samarafaqat091@gmail.com>
2024-07-21 00:28:27 +0500

    Can Place Flowers Solved

--- LeetCode 75/Arrays/6-CanPlaceFlowers.cpp
+++ LeetCode 75/Arrays/6-CanPlaceFlowers.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 bool canPlaceFlowers(vector<int>& flowerbed, int n) {
 
-    int i=0;
+   bool canPlaceFlowers(vector<int>& flowerbed, int n) {
     int size = flowerbed.size();
-    while(i < size){
-
-        int currentFlower = flowerbed[i];
-        if(i < size && flowerbed[i] == 0){
-
-
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        if(flowerbed[i] == 0) {
+            bool emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+            bool emptyRight = (i == size - 1) || (flowerbed[i + 1] == 0);
+            if(emptyLeft && emptyRight) {
+                flowerbed[i] = 1;
+                count++;
+                if(count >= n) {
+                    return true;
+                }
+            }
         }
-        i= i+n ;
-    } 
+    }
+    return count >= n;
 }
+
+} 
+
 
 int main() {
 
+    vector<int> v = {1,0,0,0,1};
+    cout<<canPlaceFlowers(v, 1);
 
     return 0;
 }
